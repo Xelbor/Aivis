60cdbecc68d7254630b4eec7883b78fcdc298aa4
Xelbor <110348877+Xelbor@users.noreply.github.com>
2023-02-01 15:38:23 +0500

    Update update.py

--- update.py
+++ update.py
@@ -3,11 +3,10 @@ import requests
 from clint.textui import progress
 
 def update_assistant():
-    voice.va_speak("После загрузки файла замените эту версию новой")
     url = 'https://github.com/Xelbor/Aivis'
     r = requests.get(url, stream=True)
 
-    with open("voice.py" "app.py" "skills.py" "words.py", "wb") as Pypdf:
+    with open("voice.py" "app.py" "skills.py" "words.py" "update.py", "wb") as Pypdf:
         total_length = int(r.headers.get('content-length'))
 
         for ch in progress.bar(r.iter_content(chunk_size=2391975),
