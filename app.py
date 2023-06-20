d3eeae014251aaf60beff2e894f96d4c09906a3a
Xelbor <110348877+Xelbor@users.noreply.github.com>
2023-06-20 11:26:52 +0500

    Update app.py
    Файл обновления Aivis V2.0

--- app.py
+++ app.py
@@ -1,196 +1,197 @@
-from sklearn.feature_extraction.text import CountVectorizer  # pip install scikit-learn
-from sklearn.linear_model import LogisticRegression
-import sounddevice as sd  # pip install sounddevice
-import vosk # pip install vosk
-
-import speech_recognition as sr
-from skills import *
-
+from common import samplerate, callback, q, model, device, commands, ui, app
+import sounddevice as sd
 import json
-import queue
-from dotenv import load_dotenv
-
-import words
 import voice
+import vosk
+import words
+from win10toast import ToastNotifier
+from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.linear_model import LogisticRegression
+import number_converter
+import skills
+import threading
+import random
+#import pymorphy2    удалено до версии 2.1 т.к из за него проблемы с компилированной версией
+import update
+import sys
 
-q = queue.Queue()
-s_city = "Kurgan,RU"
+def show_notif_update():
+    if ui.checkBox_notifications.isChecked():
+        update.get_updatings()
 
-model = vosk.Model('model_small')  # голосовую модель vosk нужно поместить в папку с файлами проекта
-# https://alphacephei.com/vosk/
-# https://alphacephei.com/vosk/models
-device = sd.default.device  # <--- по умолчанию
-# или -> sd.default.device = 1, 3, python -m sounddevice просмотр
-samplerate = int(sd.query_devices(device[0], 'input')['default_samplerate'])  # получаем частоту микрофона
-
-def search_internet():
-    # obtain audio from the microphone
-    r = sr.Recognizer()
-    with sr.Microphone() as source:
-        print("Скажите что вы хотите найти: ")
-        audio = r.listen(source)
-    try:
-        query = r.recognize_google(audio, language="ru")
-        print("Вы сказали: "+query)
-        return query
-    except sr.UnknownValueError:
-        return "ошибка"
-    except sr.RequestError:
-        return "ошибка"
-
-def commands():
-        # obtain audio from the microphone
-        r = sr.Recognizer()
-        with sr.Microphone() as source:
-            print("Скажите что вы хотите найти: ")
-            audio = r.listen(source)
-        try:
-            our_speech = r.recognize_google(audio, language="ru")
-            print("Вы сказали: " + our_speech)
-            return our_speech
-        except sr.UnknownValueError:
-            return "ошибка"
-        except sr.RequestError:
-            return "ошибка"
-
-def translate_ru():
-    # obtain audio from the microphone
-    r = sr.Recognizer()
-    with sr.Microphone() as source:
-        audio = r.listen(source)
-    try:
-        our_speech = r.recognize_google(audio, language="ru")
-        print("Вы сказали: "+our_speech)
-        return our_speech
-    except sr.UnknownValueError:
-        return "ошибка"
-    except sr.RequestError:
-        return "ошибка"
-
-
-def translate_en():
-    # obtain audio from the microphone
-    r = sr.Recognizer()
-    with sr.Microphone() as source:
-        audio = r.listen(source)
-    try:
-        our_speech = r.recognize_google(audio, language="en")
-        print("Вы сказали: "+our_speech)
-        return our_speech
-    except sr.UnknownValueError:
-        return "ошибка"
-    except sr.RequestError:
-        return "ошибка"
-
-def callback(indata, frames, time, status):
-    '''
-    Добавляет в очередь семплы из потока.
-    вызывается каждый раз при наполнении blocksize
-    в sd.RawInputStream'''
-
-    q.put(bytes(indata))
+# Показ уведомления о том что программа полностью работает
+def show_notification():
+    toaster = ToastNotifier()
+    toaster.show_toast(
+        "Айвис готов к работе",
+        "Программа запущена и готова к работе",
+        icon_path="Interface/Icon.ico",
+        duration=5
+    )
 
 def recognize(data, vectorizer, clf):
-    '''
-    Анализ распознанной речи
-    '''
-    trg = words.TRIGGERS.intersection(data.split())
+    search = ""
+    #morph = pymorphy2.MorphAnalyzer()
+    # проверяем есть ли имя бота в data, если нет, то return
+    trg = words.get_triggers().intersection(data.split())
+
+    # Если нет триггера, то продолжаем прослушивание
     if not trg:
         return
-    data.replace(list(trg)[0], '')
-    text_vector = vectorizer.transform([data]).toarray()[0]
-    answer = clf.predict([text_vector])[0]
-    func_name = answer.split()[0]
-    voice.va_speak(answer.replace(func_name, ''))
-    exec(func_name + '()')
 
+    # Проверка наличия дополнительных слов после триггера
+    if any(word in data.lower() for word in trg):
+        if len(data.split()) > len(trg):
+            # удаляем имя бота из текста
+            data = data.replace(list(trg)[0], '')
 
-def q_callback(indata, status):
-    if status:
-        print(status, file=sys.stderr)
-    q.put(bytes(indata))
+            # ищем команды для поиска в гугл, на ютубе и в википедии
+            youtube_trigger = ['найди на ютубе', 'найди на ютуб', 'найди видео на ютуб', 'найди видео', 'ищи на ютубе', 'открой на ютубе', 'включи']
+            google_trigger = ['найди в гугле', 'ищи в гугле', 'открой в гугле']
+            wiki_trigger = ['найди википедии', 'ищи в википедии', 'открой википедию', 'найди в википедии',
+                            'найди статью', 'найди статью о', 'найди статью в википедии', 'найди статью', 'что такое']
+            timer_trigger = ['поставь таймер', 'запусти таймер', 'установи таймер', 'засеки']
+            reminder_trigger = ['создай напоминание', 'сделай напоминание', 'напомни мне', 'напомни о']
+            random_numbers_trigger = ['случайное число между', 'случайное число от', 'рандомное число от',
+                                      'рандомное число между', 'генерируй случайное число от',
+                                      'генерируй случайное число между']
+            weather_trigger = ['какая сейчас погода в', 'какая сейчас погода в городе', 'какая погода на улице в', 'что там на улице в']
 
+            # Проверка для youtube_trigger
+            for trigger in youtube_trigger:
+                if trigger in data.lower():
+                    # проверяем, есть ли запрос на поиск после команды
+                    search = data.lower().replace(trigger, '').strip()
+                    if not search:
+                        skills.watch_youtube(None)
+                    # запускаем функцию для поиска видео на YouTube
+                    skills.watch_youtube(search)
+                    return
 
-def find_rec():
-    with sd.RawInputStream(samplerate=samplerate, blocksize=8000, device=device, dtype='int16',
-                           channels=1, callback=q_callback):
+            # Проверка для google_trigger
+            for trigger in google_trigger:
+                if trigger in data.lower():
+                    # получаем строку, которую нужно искать в Google
+                    search = data.lower().replace(trigger, '').strip()
+                    if not search:
+                        skills.search_for_term_on_google(None)
+                    # запускаем функцию для поиска на Google
+                    skills.search_for_term_on_google(search)
+                    return
 
-        rec = vosk.KaldiRecognizer(model, samplerate)
-        while True:
-            data = q.get()
-            if rec.AcceptWaveform(data):
-                callback(json.loads(rec.Result())["text"])
+            # Проверка для wiki_trigger
+            for trigger in wiki_trigger:
+                if trigger in data.lower():
+                    # получаем строку, которую нужно искать в Википедии
+                    query = data.lower().replace(trigger, '').strip()
+                    if not query:
+                        # если пользователь не указал запрос, запрашиваем его у пользователя
+                        voice.va_speak("Что бы вы хотели найти в Википедии?")
+                        query = commands().strip()
+                    # запускаем функцию для поиска информации в Википедии
+                    skills.search_wikipedia(query)
+                    return
+
+            # Проверка для timer_trigger
+            for trigger in timer_trigger:
+                if trigger in data.lower():
+                    # проверяем, есть ли время для таймера после команды
+                    time_set = data.lower().replace(trigger, '').strip()
+                    if not time_set:
+                        # если его нет, то спрашиваем пользователя, на сколько поставить таймер
+                        voice.va_speak("На сколько вы хотите поставить таймер?")
+                        time_set = commands()
+                    # запускаем функцию таймера
+                    skills.timer(time_set)
+                    return
+
+            # Проверка для reminder_trigger
+            for trigger in reminder_trigger:
+                if trigger in data.lower():
+                    reminder_text = data.lower().replace(trigger, '').strip()
+                    if reminder_text:
+                        if 'через' in reminder_text:
+                            text_time_list = reminder_text.split('через')
+                            if len(text_time_list) == 2:
+                                reminder_text = text_time_list[0].strip()
+                                time_set = text_time_list[1].strip()
+                                skills.reminder(time_set, reminder_text)
+                                return
+                            else:
+                                voice.va_speak("Произошла ошибка в обработке напоминания, пожалуйста, укажите время.")
+                        else:
+                            skills.reminder(None, reminder_text)
+                            return
+
+            # Проверка для random_numbers_trigger
+            for trigger in random_numbers_trigger:
+                if trigger in data.lower():
+                    # Получаем первое и второе число для генерации случайного числа в заданном диапазоне
+                    numbers = data.lower().replace(trigger, '').strip()
+                    if numbers:
+                        numbers_list = numbers.split('и')
+                        if len(numbers_list) == 2:
+                            number1 = number_converter.words_to_numbers(numbers_list[0])
+                            number1 = int(number1.strip())
+                            number2 = number_converter.words_to_numbers(numbers_list[1])
+                            number2 = int(number2.strip())
+                            if number1 > number2:
+                                number1, number2 = number2, number1
+                            skills.random_number_between(number1, number2)
+                            return
+                        else:
+                            voice.va_speak("Произошла ошибка в обработчике чисел, возможно вы указали больше чисел чем 2")
+
+            # Это код полного распознавания речи функции погоды
+
+            # Она сейчас отключена из за того что модуль pymorph
+            # выдает ошибку в компилированной версии программы. Проблема будет исправленна в версии 2.1
+
+            #for trigger in weather_trigger:
+            #    if trigger in data.lower():
+            #        # проверяем, есть ли запрос на поиск после команды
+            #        city = data.lower().replace(trigger, '').strip()
+            #        if not city:
+            #            skills.weather(None)
+            #        else:
+            #            city_normalized = morph.parse(city)[0].normal_form
+            #            skills.weather(city_normalized)
+            #            return
+
+            # получаем вектор полученного текста
+            # сравниваем с вариантами, получая наиболее подходящий ответ
+            text_vector = vectorizer.transform([data]).toarray()[0]
+            answer = clf.predict([text_vector])[0]
+            # получение имени функции из ответа из data_set
+            func_name = answer.split()[0]
+            # Довольно важные строки, они отвечают за выполнения команд
+            if func_name == "watch_youtube":
+                # Функции исключения команд, это если например после функции в скобках есть какой нибудь аргумент, то функция не будет включаться если вы не добавите в исключения
+                exec(f"{func_name}('{search}')", globals(), locals())
+            elif func_name == "search_for_term_on_google":
+                exec(f"{func_name}('{search}')", globals(), locals())
+            elif func_name == "search_wikipedia":
+                exec(f"{func_name}('{search}')", globals(), locals())
+            elif func_name == "timer":
+                exec(f"{func_name}()", globals(), locals())
+            elif func_name == "reminder":
+                exec(f"{func_name}()", globals(), locals())
+            elif func_name == "get_weather":
+                exec(f"{func_name}()", globals(), locals())
             else:
-                print(rec.PartialResult())
+                exec(f"{func_name}()", globals(), locals())
 
-#def weather():
-    #city_name = s_city
+        else:
+            # Если будет сказан только триггер, то он спросит что вы хотели? или что? или да?
+            askings = ["Что?", "Да?", "Чем могу помочь?", "Чего нибудь хотели?", "Чем могу быть полезен?"]
+            result = random.choice(askings)
+            voice.va_speak(result)
 
-    #try:
-        # использование API-ключа, помещённого в .env-файл по примеру WEATHER_API_KEY = "01234abcd....."
-       #weather_api_key = os.getenv("e6ef80c94a54fc9d869f7f9796f984f2")
-       #open_weather_map = OWM(weather_api_key)
-
-        # запрос данных о текущем состоянии погоды
-       #weather_manager = open_weather_map.weather_manager()
-       #observation = weather_manager.weather_at_place(city_name)
-       #weather = observation.weather
-
-        # поскольку все ошибки предсказать сложно, то будет произведен отлов с последующим выводом без остановки программы
-    #except:
-        #voice.va_speak("Ошибка")
-        #traceback.print_exc()
-        #return
-
-        # разбивание данных на части для удобства работы с ними
-    #status = weather.detailed_status
-    #temperature = weather.temperature('celsius')["temp"]
-    #wind_speed = weather.wind()["speed"]
-    #pressure = int(weather.pressure["press"] / 1.333)  # переведено из гПА в мм рт.ст.
-
-    # вывод логов
-    #print("Weather in " + city_name +
-                 #":\n * Status: " + status +
-                 #"\n * Wind speed (m/sec): " + str(wind_speed) +
-                 #"\n * Temperature (Celsius): " + str(temperature) +
-                 #"\n * Pressure (mm Hg): " + str(pressure), "yellow")
-
-    # озвучивание текущего состояния погоды ассистентом (здесь для мультиязычности требуется дополнительная работа)
-    #voice.va_speak(("It is {0} in {1}").format(status, city_name))
-    #voice.va_speak(("The temperature is {} degrees Celsius").format(str(temperature)))
-    #voice.va_speak(("The wind speed is {} meters per second").format(str(wind_speed)))
-    #voice.va_speak(("The pressure is {} mm Hg").format(str(pressure)))
-
-
-#def music():
-    #voice.va_speak('какую хотите включить?')
-    #mixer.music.play()
-    #mixer.music.load('E:/Voice-Helper2.0/Music/' + commands() + '.mp3')
-    #if 'стоп' in commands():
-        #mixer.music.stop()
-
-#def wikipedia():
-    #voice.va_speak('Поиск в Википедии....')
-    #our_speech = search_internet().query.replace('wikipedia', '')
-    #results = wikipedia.summary(our_speech, sentences=5)
-    #print(results)
-    #voice.va_speak(results)
-
-def random_number(command):
-    ot = commands().command.find('от')
-    do = commands().command.find('до')
-    f_num = int(commands().command[ot + 3:do - 1])
-    l_num = int(command[do + 3:])
-    r = str(random.randint(f_num, l_num))
-    print('Bot: ' + r)
-    voice.va_speak(r)
+            # если сработал только триггер, то дальше функция не выполняется
+            if len(trg) == 1 and not data.strip():
+                return
 
 def main():
-    '''
-    Обучаем матрицу ИИ
-    и постоянно слушаем микрофон
-    '''
-
     # Обучение матрицы на data_set модели
     vectorizer = CountVectorizer()
     vectors = vectorizer.fit_transform(list(words.data_set.keys()))
@@ -200,21 +201,48 @@ def main():
 
     del words.data_set
 
+    ui.label_aivis.setText("Aivis Активен")
+
     # постоянная прослушка микрофона
     with sd.RawInputStream(samplerate=samplerate, blocksize=16000, device=device[0], dtype='int16',
                            channels=1, callback=callback):
 
         rec = vosk.KaldiRecognizer(model, samplerate)
         while True:
+            lineEdit_text = ui.lineEdit_name.text()
+
+            if lineEdit_text == "":
+                ui.lineEdit_name.setText("Айви, Айвис")
+
+            with open("name.txt", "w") as w:
+                w.write(lineEdit_text)
+
             data = q.get()
             if rec.AcceptWaveform(data):
-                data = json.loads(rec.Result())["text"]
+                data = json.loads(rec.Result())['text']
                 recognize(data, vectorizer, clf)
             else:
-                print(rec.PartialResult())
+                # Преобразование строки в словарь
+                input_string = rec.PartialResult()
+                data_result = json.loads(input_string)
 
-#voice.speaker('Здравствуйте, я Айви, ваш помощник, то есть искусственный интеллект. Пожалуйста, скажите мне, как я могу вам помочь')
+                # Извлечение слова "partial"
+                result = data_result["partial"]
 
-if __name__ == '__main__':
-    load_dotenv()
-    main()
+                # Выводим результат с микрофона
+                print(result)
+                # Выводим результат с микрофона на интерфейс
+                ui.label_print_voice.setText(result)
+
+if __name__ == "__main__":
+    # Создаем потоки для прослушивания микрофона и отправки уведомления
+    mic_thread = threading.Thread(target=main)
+    notif_thread = threading.Thread(target=show_notification)
+    update_notif_thread = threading.Thread(target=show_notif_update)
+
+    # Запускаем потоки
+    update_notif_thread.start()
+    mic_thread.start()
+    notif_thread.start()
+
+    sys.exit(app.exec_())
